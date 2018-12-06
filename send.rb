b4df33792032bd552758a31c26610da347649ca3
Richard <richie1988@gmail.com>
2018-12-05 22:07:34 -0500

    first commit

---
+++ send.rb
@@ -0,0 +1,27 @@
+require 'action_mailer'
+
+ActionMailer::Base.view_paths=File.dirname(__FILE__)
+ActionMailer::Base.smtp_settings = {
+  address:        'smtp.gmail.com',
+  port:           '587',
+  user_name:      ENV['EMAIL_USERNAME'],
+  password:       ENV['EMAIL_PASSWORD'],
+  authentication: :plain
+}
+
+class Newsletter < ActionMailer::Base
+  default from: 'richard@example.com'
+  def welcome(recipient)
+    @recipient = recipient
+    mail(to: recipient,
+        subject: "[Signed up] Welcome #{recipient}")
+  end
+end
+
+# Newsletter.welcome('richie1988@gmail.com').deliver_now
+
+# You need a fodler whose name matches the name of the class inheriting from
+# actionmailer base
+
+# In that folder:
+# you need at least 2 files: (function).html.erb AND (function).text.erb
---
+++ server.rb
@@ -0,0 +1,22 @@
+load 'send.rb'
+require 'sinatra'
+
+get '/' do
+  erb :landing
+end
+
+get '/index' do
+  erb :index
+end
+
+get '/cakes' do
+  erb :cakes
+end
+
+get '/cookies' do
+  erb :cakes
+end
+
+get '/muffins' do
+  erb :cakes
+end
